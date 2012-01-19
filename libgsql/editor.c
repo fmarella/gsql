ea46c05372134d54849eeffd4e3be60213d9272d
Francesco Marella <francesco.marella@gmail.com>
2012-01-19 21:16:40 +0100

    Disable deprec gtk_source_view_set_mark_category_pixbuf

--- libgsql/editor.c
+++ libgsql/editor.c
@@ -181,6 +181,7 @@ gsql_source_editor_new(gchar * buffer_body)
 
 	gtk_source_buffer_set_highlight_syntax (buffer, TRUE);
 	gsql_source_editor_property_set (source);
+/*	Deprecated ?
 	
 	gtk_source_view_set_mark_category_pixbuf (GTK_SOURCE_VIEW (source), GSQL_EDITOR_MARKER_COMPLETE,
 									   // FIXME. make another icon.
@@ -188,6 +189,7 @@ gsql_source_editor_new(gchar * buffer_body)
 	gtk_source_view_set_mark_category_pixbuf (GTK_SOURCE_VIEW (source), GSQL_EDITOR_MARKER_FAILED,
 									   // FIXME. make another icon.
 									   create_pixbuf ("msg_error.png"));
+*/
 	gtk_source_view_set_show_line_marks (GTK_SOURCE_VIEW (source), TRUE);	
 	
 
